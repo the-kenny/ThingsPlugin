a87ed87a80fe07d46febd29a6b2db07c5775a8eb
Moritz Ulrich <thekenny123@gmail.com>
2009-07-01 17:23:37 +0200

    The date is now shown in the current locale

--- plugin.js
+++ plugin.js
@@ -5,17 +5,26 @@ function ThingsPlugin()
 ThingsPlugin.prototype.bundleIdentifier="cx.ath.the-kenny.ThingsPlugin";
 ThingsPlugin.prototype.updateView = function(todo)
 {
+  var unixTimeNewYear2001 = 978307200;
+
   var todos = todo.todos;
 
   if (todos.length > 0)
 	{
-	  var html = "<ul><li class='header'>New ToDo" + 
-		((todos.length==1) ? "" : "s") + ": " +
+	  var html = "<ul><li class='header'>ToDo" + 
+		((todos.length==1) ? "" : "s") + " today: " +
 		todos.length+"</li>";
 
 		for (i = 0; i < todos.length; i++) {
 			html += "<li class='summary"+(i == 0 ? " firstItem" : "")+(i == todos.length - 1 ? " lastItem" : "")+"'>"+todos[i].text+"</li>";
-			html += "<li class='location'>"+todos[i].due+"</li>";
+
+			if(todos[i].due) {
+			  var date = new Date();
+			  date.setTime((unixTimeNewYear2001 + parseInt(todos[i].due)) * 1000);
+			  html += "<li class='location'>"+date.toLocaleDateString()+"</li>";
+			} else {
+			  html += "<li class='location'> </li>";
+			}
 		}
 
 		html += "</ul>";
