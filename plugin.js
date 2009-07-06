0970c29591c9764d0b2cd0107734210e2b3a23ca
Moritz Ulrich <thekenny123@gmail.com>
2009-07-06 16:29:10 +0200

    Re-indented the code

--- plugin.js
+++ plugin.js
@@ -3,41 +3,39 @@ function ThingsPlugin()
 }
 
 ThingsPlugin.prototype.bundleIdentifier="cx.ath.the-kenny.ThingsPlugin";
-ThingsPlugin.prototype.updateView = function(todo)
-{
+ThingsPlugin.prototype.updateView = function(todo) {
   var unixTimeNewYear2001 = 978307200;
 
   var todos = todo.todos;
 
-  if (todos.length > 0)
-	{
-	  var html = "<ul><li class='header'>ToDo" + 
-		((todos.length==1) ? "" : "s") + " today:</li>";
+  if (todos.length > 0) {
+	var html = "<ul><li class='header'>ToDo" + 
+	  ((todos.length==1) ? "" : "s") + " today:</li>";
 
-	  for (i = 0; i < todos.length; i++) {
-			html += "<li class='summary"+(i == 0 ? " firstItem" : "")+(i == todos.length - 1 ? " lastItem" : "")+"'>"+todos[i].text+"</li>";
+	for (i = 0; i < todos.length; i++) {
+	  html += "<li class='summary"+(i == 0 ? " firstItem" : "")+(i == todos.length - 1 ? " lastItem" : "")+"'>"+todos[i].text+"</li>";
 
-			if(todos[i].due) {
-			  var date = new Date();
-			  date.setTime((unixTimeNewYear2001 + parseInt(todos[i].due)) * 1000);
-			  html += "<li class='location'>Due "+date.toLocaleDateString()+"</li>";
-			} else {
-			  html += "<li class='location'> </li>";
-			}
-		}
-
-	  /*
-	  for(var prop in todo)
-		html += "<li>" + prop + "</li>";
-	  */
-
-		html += "</ul>";
-		getPluginDiv(this).className = "things";
-		getPluginDiv(this).innerHTML = html;
-		
+	  if(todos[i].due) {
+		var date = new Date();
+		date.setTime((unixTimeNewYear2001 + parseInt(todos[i].due)) * 1000);
+		html += "<li class='location'>Due "+date.toLocaleDateString()+"</li>";
+	  } else {
+		html += "<li class='location'> </li>";
+	  }
 	}
-  else
+
+	/*
+	  for(var prop in todo)
+	  html += "<li>" + prop + "</li>";
+	*/
+
+	html += "</ul>";
+	getPluginDiv(this).className = "things";
+	getPluginDiv(this).innerHTML = html;
+		
+  } else {
   	getPluginDiv(this).innerHTML = "";
+  }
 
   return true;
 }
